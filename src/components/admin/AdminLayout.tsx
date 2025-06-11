38b25bef8d91ac20b9bd87e9524f7809a4a976f3
Muhammad Yasir Ismail <yasir@lmntrix.com>
2025-06-11 13:38:15 +0500

    Scroll issue fixed

--- src/components/admin/AdminLayout.tsx
+++ src/components/admin/AdminLayout.tsx
@@ -38,7 +38,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
           </div>
         </header>
         {/* Page Content */}
-        <main className="flex-1 p-6">
+        <main className="flex-1 overflow-y-auto p-6 max-h-[87vh]">
         
           {children}
         </main>
